2f4fc84f993462574d5cd7b3627e4337165d1ba9
JoshTheSnom <filip.makrlik@gmail.com>
2025-02-08 17:39:22 +0100

    Loaded data from API. Had to bypass CORS in backend.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { useState, useEffect } from 'react';
+
+const API_URL = 'http://localhost:5178/data'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [table, setTable] = useState([]);
+    useEffect(() => {
+        fetch(API_URL)
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setTable(data);
+            });
+    }, []);
 
-  return (
+
+    return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div>
+            <p>Lorem ipsum</p>
+        </div>
+
+        <div>
+            <pre>{JSON.stringify(table, null, 1)}</pre>
+        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Label</th>
+                    <th>Datum</th>
+                    <th>Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                {/* table content */}
+
+                </tr>
+            </tbody>
+
+        </table>
     </>
   )
 }
