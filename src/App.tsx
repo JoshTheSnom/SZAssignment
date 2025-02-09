3f85f60187a0e1db618ac61325cc944df5e024a7
JoshTheSnom <filip.makrlik@gmail.com>
2025-02-09 18:58:06 +0100

    Replaced table with MUI table

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-
 import './App.css'
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import {createTheme, ThemeProvider, styled} from '@mui/material/styles';
 
 const API_URL = 'http://localhost:5178/data'
+const darkTheme = createTheme({palette: {mode: 'dark'}});
 
 export interface TableData {
     id: number
@@ -27,37 +35,40 @@ function App() {
 
 
     return (
-    <>
-        <div>
-            <p>Lorem ipsum</p>
-        </div>
+        <>
+            <div>
+                <p>Lorem ipsum</p>
+            </div>
+            <div>
+                {/*<pre>{JSON.stringify(table, null, 2)}</pre>*/}
+            </div>
+            <ThemeProvider theme={darkTheme}>
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>ID</TableCell>
+                                <TableCell>Label</TableCell>
+                                <TableCell>Datum</TableCell>
+                                <TableCell>Name</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {table.map((item, index) => (
+                                <TableRow key={index}>
+                                    <TableCell>{item.id}</TableCell>
+                                    <TableCell>{item.label}</TableCell>
+                                    <TableCell>{item.datum}</TableCell>
+                                    <TableCell>{item.name}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </ThemeProvider>
 
-        <div>
-            {/*<pre>{JSON.stringify(table, null, 2)}</pre>*/}
-        </div>
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Label</th>
-                    <th>Datum</th>
-                    <th>Name</th>
-                </tr>
-            </thead>
-            <tbody>
-                {table.map(( item, index) => (
-                    <tr key={index}>
-                        <td>{item.id}</td>
-                        <td>{item.label}</td>
-                        <td>{item.datum}</td>
-                        <td>{item.name}</td>
-                    </tr>
-                    ))}
-            </tbody>
-
-        </table>
-    </>
-  )
+        </>
+    )
 }
 
 export default App
