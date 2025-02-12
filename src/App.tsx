dd414964c78c991e78f12dfd6f9b98995874362b
JoshTheSnom <filip.makrlik@gmail.com>
2025-02-12 19:16:40 +0100

    Made buttons component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import "./App.css";
-import FileDownloadIcon from "@mui/icons-material/FileDownload";
-import SearchIcon from "@mui/icons-material/Search";
-import { Button, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import "dayjs/locale/cs";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -9,8 +7,8 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import type { Dayjs } from "dayjs";
 import dayjs from "dayjs";
 import { useEffect, useState } from "react";
-import { CSVLink } from "react-csv";
 import * as XLSX from "xlsx";
+import Buttons from "./components/Buttons.tsx";
 import DataTable from "./components/DataTable.tsx";
 import DateFilters from "./components/DateFilters.tsx";
 
@@ -24,6 +22,11 @@ export interface TableData {
 	name: string | null;
 }
 
+export interface Header {
+	label: string;
+	key: string;
+}
+
 function App() {
 	dayjs.locale("cs");
 
@@ -33,7 +36,7 @@ function App() {
 	const [toDate, setToDate] = useState<Dayjs | undefined>(undefined);
 	const [fromError, setFromError] = useState<string | null>(null);
 	const [toError, setToError] = useState<string | null>(null);
-	const [isFiltered, setIsFiltered] = useState<boolean | null>(false);
+	const [isFiltered, setIsFiltered] = useState<boolean>(false);
 
 	const handleSearch = () => {
 		const filtered = table.filter((item) => {
@@ -72,7 +75,7 @@ function App() {
 		XLSX.writeFile(workbook, "exported_data.xlsx");
 	};
 
-	const headers = [
+	const headers: Header[] = [
 		{ label: "ID", key: "id" },
 		{ label: "Label", key: "label" },
 		{ label: "Datum", key: "datum" },
@@ -98,49 +101,19 @@ function App() {
 								}}
 							/>
 
-							<Stack
-								direction="row"
-								paddingBottom={3}
-								spacing={2}
-								//sx={{ paddingBottom: "20px" }}
-							>
-								<Button
-									size="small"
-									variant="contained"
-									endIcon={<FileDownloadIcon />}
-									component="span"
-									sx={{ height: "100%" }} //cheap fix, but it works
-									onClick={exportToXLSX}
-									disabled={!isFiltered}
-								>
-									.xlsx
-								</Button>
-								<CSVLink
-									data={filteredTable}
-									headers={headers}
-									filename={"exported_data.csv"}
-								>
-									<Button
-										size="small"
-										variant="contained"
-										endIcon={<FileDownloadIcon />}
-										component="span"
-										sx={{ height: "100%" }} //cheap fix, but it works
-										disabled={!isFiltered}
-									>
-										.csv
-									</Button>
-								</CSVLink>
-								<Button
-									size="small"
-									variant="outlined"
-									endIcon={<SearchIcon />}
-									onClick={handleSearch}
-									disabled={!!fromError || !!toError || (!toDate && !fromDate)}
-								>
-									Search
-								</Button>
-							</Stack>
+							<Buttons
+								{...{
+									filteredTable,
+									headers,
+									isFiltered,
+									fromError,
+									toError,
+									fromDate,
+									toDate,
+									exportToXLSX,
+									handleSearch,
+								}}
+							/>
 						</Stack>
 						<DataTable data={filteredTable} />
 					</Stack>
---
+++ src/components/Buttons.tsx
@@ -0,0 +1,81 @@
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import SearchIcon from "@mui/icons-material/Search";
+import { Button, Stack } from "@mui/material";
+import type { Dayjs } from "dayjs";
+import type React from "react";
+import { CSVLink } from "react-csv";
+import type { Header, TableData } from "../App.tsx";
+
+interface ButtonsProps {
+	filteredTable: TableData[];
+	headers: Header[];
+	isFiltered: boolean;
+	fromError: string | null;
+	toError: string | null;
+	fromDate: Dayjs | undefined;
+	toDate: Dayjs | undefined;
+	exportToXLSX: () => void;
+	handleSearch: () => void;
+}
+const Buttons: React.FC<ButtonsProps> = ({
+	filteredTable,
+	headers,
+	isFiltered,
+	fromError,
+	toError,
+	fromDate,
+	toDate,
+	exportToXLSX,
+	handleSearch,
+}) => {
+	return (
+		<>
+			<Stack
+				direction="row"
+				paddingBottom={3}
+				spacing={2}
+				//sx={{ paddingBottom: "20px" }}
+			>
+				<Button
+					size="small"
+					variant="contained"
+					endIcon={<FileDownloadIcon />}
+					component="span"
+					sx={{ height: "100%" }} //cheap fix, but it works
+					onClick={exportToXLSX}
+					disabled={!isFiltered}
+				>
+					.xlsx
+				</Button>
+				<CSVLink
+					data={filteredTable}
+					headers={headers}
+					separator={";"}
+					filename={"exported_data.csv"}
+				>
+					<Button
+						size="small"
+						variant="contained"
+						endIcon={<FileDownloadIcon />}
+						component="span"
+						sx={{ height: "100%" }} //cheap fix, but it works
+						disabled={!isFiltered}
+					>
+						.csv
+					</Button>
+				</CSVLink>
+				<Button
+					size="small"
+					variant="outlined"
+					endIcon={<SearchIcon />}
+					onClick={handleSearch}
+					disabled={!!fromError || !!toError || (!toDate && !fromDate)}
+				>
+					Search
+				</Button>
+			</Stack>
+		</>
+	);
+};
+
+export default Buttons;
